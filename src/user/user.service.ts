7a7c581edb7950de99b5ca4b4472ef5f7ad4da96
Daniel Ferreira <silva_120000@yahoo.com.br>
2024-07-16 15:50:00 -0300

    creating user module

---
+++ src/user/user.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  prisma = new PrismaClient();
+  async create(createUserDto: CreateUserDto) {
+    const saltOrRounds = 10;
+    const hash = await bcrypt.hash(createUserDto.password, saltOrRounds);
+    const user = this.prisma.user.findFirst({
+      where: { email: createUserDto.email },
+    });
+    if (user) {
+      throw new Error('Email already exists in the database');
+    }
+
+    this.prisma.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password: hash,
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
